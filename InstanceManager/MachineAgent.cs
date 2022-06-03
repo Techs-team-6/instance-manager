ccde1d3c1cf2850a7461cf9a1491122364949013
1martin1 <cuatrocosmos@gmail.com>
2022-06-03 14:26:40 +0300

    hjm

--- InstanceManager/MachineAgent.cs
+++ InstanceManager/MachineAgent.cs
@@ -1,4 +1,6 @@
-﻿using System.IO.Compression;
+﻿using System.Diagnostics;
+using System.IO.Compression;
+using DMConnect.Client;
 using DMConnect.Share;
 using Domain.Dto.DedicatedMachineDto;
 using InstanceManager.DownloadFiles;
@@ -16,10 +18,43 @@ public class MachineAgent : IDedicatedMachineAgent
         _download = new Download();
     }
 
+    public IDedicatedMachineHub Hub { get; set; }
+
     public void StartInstance(StartInstanceDto dto)
     {
         string fileName = dto.InstanceId.ToString() + "/build.zip";
         _download.DownloadFile(dto.BuildUrl, fileName);
-        ZipFile.ExtractToDirectory(fileName,dto.InstanceId.ToString());
+        ZipFile.ExtractToDirectory(fileName, dto.InstanceId.ToString());
+
+        try
+        {
+            using (Process myProcess = new Process())
+            {
+                myProcess.StartInfo.UseShellExecute = false;
+                // You can start any process, HelloWorld is a do-nothing example.
+                myProcess.StartInfo.FileName = dto.InstanceId.ToString() + '/' + dto.StartScript;
+                myProcess.StartInfo.CreateNoWindow = true;
+                var streamReader = myProcess.StandardOutput;
+                myProcess.Start();
+                string? line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+                /*Console.WriteLine("PagedMemorySize64: "+myProcess.PagedMemorySize64);
+                Console.WriteLine("PrivateMemorySize64: "+myProcess.PrivateMemorySize64);
+                Console.WriteLine("VirtualMemorySize64: "+myProcess.VirtualMemorySize64);
+                Console.WriteLine("NonpagedSystemMemorySize64: "+myProcess.NonpagedSystemMemorySize64);
+                Console.WriteLine("PagedSystemMemorySize64: "+myProcess.PagedSystemMemorySize64);
+                Console.WriteLine("PeakPagedMemorySize64: "+myProcess.PeakPagedMemorySize64);
+                Console.WriteLine("PeakVirtualMemorySize64: "+myProcess.PeakVirtualMemorySize64);
+                Console.WriteLine("PeakWorkingSet64: "+myProcess.PeakWorkingSet64);
+                Console.WriteLine("WorkingSet64: "+myProcess.WorkingSet64);*/
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
     }
 }
--- InstanceManager/Program.cs
+++ InstanceManager/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using System.Net;
 using DMConnect.Client;
+using Domain.Dto.DedicatedMachineDto;
 using InstanceManager.DownloadFiles;
 using InstanceManager.Procesess;
 using InstanceManager.SystemInfo;
@@ -11,9 +12,26 @@ namespace InstanceManager
     {
         static void Main(string[] args)
         {
-            /*var endPoint = new IPEndPoint(new IPAddress())
-            var client = new RemoteDedicatedMachineHub()*/
-            
+            var machineAgent = new MachineAgent();
+            var endPoint = new IPEndPoint(Dns.GetHostByName(args[0]).AddressList[0], int.Parse(args[1]));
+            var token = args[2];
+            var fileName = "machineId.txt";
+            RemoteDedicatedMachineHub client;
+            if (File.Exists(fileName))
+            {
+                var id = Guid.Parse(File.ReadAllText(fileName));
+                client = new RemoteDedicatedMachineHub(endPoint, machineAgent, new AuthDto(id, token));
+            }
+            else
+            {
+                client = new RemoteDedicatedMachineHub(endPoint, machineAgent, 
+                    new RegisterDto(token, Environment.OSVersion.ToString(), "Description is cool, but no"));
+                File.WriteAllText(fileName, client.MachineId.ToString());
+            }
+
+            machineAgent.Hub = client;
+            client.Start();
+
             /*ProcesInformation procesInformation = new ProcesInformation();
             procesInformation.GetProcessInformation();
 
