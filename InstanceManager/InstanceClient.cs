d10aac7c68813cd7e5bd60fc0a77a41f79e0f64c
Lumograul <lumograul@gmail.com>
2022-06-22 23:53:13 +0300

    feat: read and send stdOut and stdErr async feat: implement InstanceClient.Stop() method refactor: make InstanceClient.Start async

--- InstanceManager/InstanceClient.cs
+++ InstanceManager/InstanceClient.cs
@@ -1,27 +1,49 @@
 using System.Diagnostics;
 using System.IO.Compression;
+using DMConnect.Share;
+using Domain.Dto.DedicatedMachineDto;
 using InstanceManager.Services;
+using ThreadState = System.Threading.ThreadState;
 
 namespace InstanceManager;
 
 public class InstanceClient
 {
-    public readonly Guid InstanceId;
-    public readonly string BuildUrl;
-    public readonly string StartScript;
+    private readonly Guid InstanceId;
+    private readonly string BuildUrl;
+    private readonly string StartScript;
     private readonly DownloadService _downloadService;
+    private readonly Thread _thread;
+    private readonly IDedicatedMachineHub _hub;
+    private readonly CancellationTokenSource _cancellationToken;
 
-    public InstanceClient(Guid instanceId, string buildUrl, string startScript, DownloadService downloadService)
+    public InstanceClient(Guid instanceId, string buildUrl, string startScript, DownloadService downloadService,
+        IDedicatedMachineHub hub)
     {
         InstanceId = instanceId;
         BuildUrl = buildUrl;
         StartScript = startScript;
         _downloadService = downloadService;
+        _hub = hub;
+        _thread = new Thread(Launch);
+        _cancellationToken = new CancellationTokenSource();
     }
 
     public void Start()
     {
-        string fileName = InstanceId.ToString() + "/build.zip";
+        _thread.Start();
+    }
+
+    public void Stop()
+    {
+        _cancellationToken.Cancel();
+        if (_thread.ThreadState != ThreadState.Unstarted)
+            _thread.Join();
+    }
+
+    private void Launch()
+    {
+        var fileName = Path.Combine(InstanceId.ToString(), "build.zip");
         _downloadService.DownloadFile(BuildUrl, fileName);
         ZipFile.ExtractToDirectory(fileName, InstanceId.ToString());
 
@@ -31,13 +53,18 @@ public class InstanceClient
             myProcess.StartInfo.UseShellExecute = false;
             myProcess.StartInfo.FileName = InstanceId.ToString() + '/' + StartScript;
             myProcess.StartInfo.CreateNoWindow = true;
-            var streamReader = myProcess.StandardOutput;
+
+            var threadOut = CreateStreamResendingThread(myProcess.StandardOutput,
+                line => _hub.InstanceStdOut(new InstanceStdOutDto(InstanceId, line)));
+            threadOut.Start();
+
+            var threadError = CreateStreamResendingThread(myProcess.StandardError,
+                line => _hub.InstanceStdErr(new InstanceStdErrDto(InstanceId, line)));
+            threadError.Start();
+
             myProcess.Start();
-            string? line;
-            while ((line = streamReader.ReadLine()) != null)
-            {
-                Console.WriteLine(line);
-            }
+            var task = myProcess.WaitForExitAsync();
+            task.Wait(_cancellationToken.Token);
         }
         catch (Exception e)
         {
@@ -45,8 +72,15 @@ public class InstanceClient
         }
     }
 
-    public void Stop()
+    private static Thread CreateStreamResendingThread(TextReader reader, Action<string> action)
     {
-        //TODO implement
+        return new Thread(() =>
+        {
+            string? line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                action.Invoke(line);
+            }
+        });
     }
 }
